491d2dc9ed3725de522aa4f859f07652a2387689
Kevin Zweerink <kevinzweerink@gmail.com>
2019-06-03 12:01:57 -0500

    v1

--- script.js
+++ script.js
@@ -1,129 +1,93 @@
-const rotatorValues = [
-  '10:04 AM EST',
-  '73 degrees fahrenheit',
-  'Mostly sunny',
-  'Working',
-  'Saturday, June 1, 2019'
-];
-
-let defaultRotator = 'A design and development studio.';
-
-let count = 0;
-
-const swapRotator = () => {
-  if (count % 2 == 0) {
-    rotator.innerHTML = defaultRotator
-  } else {
-    rotator.innerHTML = rotatorValues[Math.floor(Math.random() * rotatorValues.length)]
-  }
-
-  count++;
+var SlideShow = function (el) {
+  this.container = el;
+  this.slides = el.querySelectorAll('.slide');
+  this.index = 0;
+  this.slideCount = this.slides.length;
 }
 
-const rotator = document.querySelector('.rotator');
-const interval = setInterval(swapRotator, 3000);
-const intro = document.querySelector('.intro')
+SlideShow.prototype.init = function () {
+  this.container.addEventListener('click', () => { 
+    clearInterval(this.interval)
+    this.interval = setInterval(() => { this.advance() }, 5000)
+    this.advance() 
+  });
 
-const W = window.innerWidth;
-const H = window.innerHeight;
-
-const introCards = [
-  {
-    type: 'image',
-    value: 'addy_01.jpg'
-  },
-  {
-    type: 'image',
-    value: 'addy_02.jpg'
-  },
-  {
-    type: 'image',
-    value: 'ff_01.jpg'
-  },
-  {
-    type: 'image',
-    value: 'ff_02.jpg'
-  },
-  {
-    type: 'image',
-    value: 'ff_03.jpg'
-  },
-  {
-    type: 'image',
-    value: 'iz_04.jpg'
-  },
-  {
-    type: 'image',
-    value: 'iz_01.jpg'
-  },
-  {
-    type: 'image',
-    value: 'iz_02.jpg'
-  },
-  {
-    type: 'image',
-    value: 'iz_03.jpg'
-  },
-  {
-    type: 'image',
-    value: 'tc_01.jpg'
-  },
-  {
-    type: 'text',
-    value: 'We design and build inventive solutions to tricky problems.'
-  },
-
-];
-
-const textCard = text => {
-  var c = document.createElement('div');
-  c.classList.add('intro-card__text');
-  c.classList.add('intro-card')
-
-  var p = document.createElement('p');
-  p.innerHTML = text;
-
-  c.appendChild(p);
-  return c;
-}
-
-const imageCard = image => {
-  var c = document.createElement('div');
-  c.classList.add('intro-card');
-  c.classList.add('intro-card__image');
-
-  var i = document.createElement('img');
-  i.src = './assets/' + image;
-
-  c.appendChild(i);
-  return c;
-}
-
-const placeIntroCards = cards => {
-  cards.forEach(card => {
-    let c = false;
-    let size = H * .6;
-    if (card.type === 'text') {
-      c = textCard(card.value);
-      size = 500;
-    }
-
-    if (card.type === 'image') {
-      c = imageCard(card.value)
-      c.style.zIndex = Math.floor(Math.random() * 200);
-    }
-
-    const targetW = (W - size - 100);
-    const unit = Math.floor(Math.random() * 5) / 5;
-
-    c.style.width = size + 'px';
-    c.style.left = Math.random() * 40 - 20 + 50 + '%';
-    c.style.top = Math.random() * 40 - 20 + 50 + '%';
-
-    if (c) {
-      intro.appendChild(c);
+  window.addEventListener('keyup', (e) => {
+    if (e.keyCode == 39) {
+      clearInterval(this.interval)
+      this.interval = setInterval(() => { this.advance() }, 5000)
+      this.advance()
+    } else if (e.keyCode == 37) {
+      clearInterval(this.interval)
+      this.interval = setInterval(() => { this.advance() }, 5000)
+      this.reverse()
     }
   })
+  this.interval = setInterval(() => { this.advance() }, 5000)
+  this.setState(this.index);
 }
 
-placeIntroCards(introCards);
+SlideShow.prototype._slideIndexForIndex = function (i) {
+  return Math.abs(i % this.slideCount);
+}
+
+SlideShow.prototype.setState = function (index) {
+  const i = this._slideIndexForIndex(index);
+  const pi = this._slideIndexForIndex(index - 1)
+  const ni = this._slideIndexForIndex(index + 1)
+  const c = this.slides[i];
+  const p = this.slides[pi];
+  const n = this.slides[ni];
+
+  this.slides.forEach(slide => { slide.className = 'slide hidden' });
+
+  c.className = 'slide current';
+  p.className = 'slide prev';
+  n.className = 'slide next';
+}
+
+SlideShow.prototype.advance = function () {
+  this.index++;
+  this.setState(this.index)
+}
+
+SlideShow.prototype.reverse = function () {
+  this.index--;
+  this.setState(this.index);
+}
+
+const ss = new SlideShow(document.querySelector('.slideshow'));
+ss.init();
+
+var Animator = function () {
+  this.tasks = [];
+}
+
+Animator.prototype.loop = function () {
+  this.tasks.forEach(task => {
+    task();
+  });
+  window.requestAnimationFrame(() => {
+    this.loop();
+  });
+}
+
+var ani = new Animator();
+
+const windowHeight = window.innerHeight;
+const scrollReveals = document.querySelectorAll('.scroll-reveal');
+
+ani.tasks.push(() => {
+  const st = -1 * window.scrollY;
+
+  scrollReveals.forEach(sr => {
+    const top = sr.getBoundingClientRect().top
+    if (top < windowHeight * .85) {
+      sr.classList.add('in-view');
+    } else {
+      sr.classList.remove('in-view');
+    }
+  })
+});
+
+ani.loop();
