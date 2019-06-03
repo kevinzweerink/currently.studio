e5000ea462e611f48dd0ed769dfa51552843bac4
Kevin Zweerink <kevinzweerink@gmail.com>
2019-06-03 16:19:07 -0500

    add slide info

--- script.js
+++ script.js
@@ -91,3 +91,9 @@ ani.tasks.push(() => {
 });
 
 ani.loop();
+
+console.log(`
+Thanks for stopping by!
+
+This site is just built with plain old HTML, CSS, and Javascript, but we're comfortable with modern development tools like React, Vue, Webpack, etc.
+`)
