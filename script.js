c9523b34d9a193c743a2ef63e1f74541de34cfc4
Kevin <kevinzweerink@gmail.com>
2019-07-18 11:37:52 -0400

    cleanup

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-console.log(`
+(function () {
+  console.log(`
 Thanks for stopping by!
 
 This site is just built with plain old HTML, CSS, and Javascript, but we're comfortable with modern development tools like React, Vue, Webpack, etc.
 `)
+})()
